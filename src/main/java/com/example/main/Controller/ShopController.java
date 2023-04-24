6901c984fd9b2b739ca95c886d8ef394d0751d1e
Priyu714 <priyankarahinj714@gmail.com>
2023-04-24 12:41:14 +0530

    put and delete mapping added

--- src/main/java/com/example/main/Controller/ShopController.java
+++ src/main/java/com/example/main/Controller/ShopController.java
@@ -14,11 +14,14 @@ public class ShopController {
     @Autowired
     ShopRepository shopRepository;
 
+    //localhost:9090/shop/AllShop
     @GetMapping("AllShop")
     public List<Shop> getAllShop()
     {
         return shopRepository.findAll();
     }
+
+    //localhost:9090/shop/shopbyid/12
     @GetMapping("shopbyid/id")
     public Shop getAllShopById(@PathVariable String id)
     {
@@ -26,11 +29,26 @@ public class ShopController {
     }
 
 
+    //localhost:9090/shop/saveshop
     @PostMapping("/saveshop")
     public Shop saveStore(@RequestBody Shop shop) {
         shopRepository.save(shop);
         return shop;
     }
 
+    //localhost:9090/shop/updateShop/12
+    @PutMapping("updateShop/{id}")
+    public Shop updateShopById(@RequestBody Shop shop,@PathVariable String id )
+    {
+        return shopRepository.save(shop);
+    }
+
+    //localhost:9090/shop/deleteshopid/24
+    @DeleteMapping("deleteshopid/{id}")
+    public String deleteShopById(@PathVariable String id)
+    {
+        shopRepository.deleteById(id);
+        return "Shop Id is successfully deleted.";
+    }
 
 }
